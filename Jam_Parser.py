501ff2ed7e9a7c84eff520a92e3239bb720dc3ba
Yuri <caesgou@mail.ru>
2025-03-22 13:47:00 +0500

    Updated files

--- Jam_Parser.py
+++ Jam_Parser.py
@@ -6,8 +6,10 @@ html = requests.get("https://www.russianfood.com/recipes/bytype/?fid=91&ysclid=m
 r_title = re.findall(r"<h3 itemprop=\"name\">(.+)</h3>", html)
 r_subtitle = re.findall(r"<div class=\"announce \" itemprop=\"description\">\s+<p>(.+)</p>\s+</div>", html)
 r_src = re.findall(r"<div>\s+<img class=\"round shadow\" src=(.+)\s+title=.+\s+alt=.+\s+itemprop=.+>\s+</div>", html)
+r_products = re.findall(r"<div class=\"announce_sub\"><span>Продукты:&nbsp;(.+)</span>\s+</div>", html)
 counter_n = 0
 counter_i = 0
+counter_k = 0
 
 with open("jams.txt", "w", encoding="utf-8") as f:
     for index, t in enumerate(r_title):
@@ -19,8 +21,13 @@ with open("jams.txt", "w", encoding="utf-8") as f:
         f.write("Sub-Title: ")
         for i in r_subtitle[counter_i - 1]:
             f.write(i)
+        f.write("\n")
+        f.write("Products: ")
+        for k in r_products[counter_k - 1]:
+            f.write(k)
         counter_n += len(n)
         counter_i += len(i)
+        counter_k += len(k)
         if index < len(r_title) - 1:
             f.write("\n\n")
 f.close()
