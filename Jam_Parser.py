04f7275b9c9f42cf8dbea9c56216c723e17db95a
Yuri <caesgou@mail.ru>
2025-03-24 14:12:12 +0500

    Full working version

--- Jam_Parser.py
+++ Jam_Parser.py
@@ -1,12 +1,21 @@
 import requests
 import re
 
-html = requests.get("https://www.russianfood.com/recipes/bytype/?fid=91&ysclid=m8jsskezk122439401").text
+html = requests.get(
+    "https://www.russianfood.com/recipes/bytype/?fid=91&page=8#rcp_list"
+).text
 
-r_title = re.findall(r"<h3 itemprop=\"name\">(.+)</h3>", html)
-r_products = re.findall(r"<div class=\"announce_sub\"><span>Продукты:&nbsp;(.+)</span>\s+</div>", html)
-r_subtitle = re.findall(r"<div class=\"announce \" itemprop=\"description\">\s+<p>(.+)</p>\s+</div>", html)
-r_src = re.findall(r"<div>\s+<img class=\"round shadow\" src=(.+)\s+title=.+\s+alt=.+\s+itemprop=.+>\s+</div>", html)
+r_title = re.findall(
+    r"<div class=\"\">\s+<div class=\"title\"><h3>(.+)</h3></div>\s+</div>", html
+)
+r_products = re.findall(
+    r"<div class=\"announce_sub\"><span>Продукты:&nbsp;(.+)</span>\s+</div>", html
+)
+r_subtitle = re.findall(r"<div class=\"announce \">\s+<p>(.+)</p>\s+</div>", html)
+r_src = re.findall(
+    r"<div>\s+<img class=\"round shadow\" src=\"(.+)\" title=\".+\"\s+alt=\".+\">\s+</div>",
+    html,
+)
 
 counter_k = 0
 counter_i = 0
@@ -16,22 +25,22 @@ with open("jams.txt", "w", encoding="utf-8") as f:
     for index, t in enumerate(r_title):
         f.write(f"Title: {t}")
         f.write("\nProducts: ")
-        
+
         for k in r_products[counter_k]:
             f.write(k)
         f.write("\nSub-Title: ")
-        
-        for i in r_subtitle[counter_i - 1]:
+
+        for i in r_subtitle[counter_i]:
             f.write(i)
         f.write("\nSCR: ")
-        
-        for n in r_src[counter_n - 1]:
+
+        for n in r_src[counter_n]:
             f.write(n)
-        
+
         counter_k += len(k)
         counter_i += len(i)
         counter_n += len(n)
-        
+
         if index < len(r_title) - 1:
             f.write("\n\n")
 f.close()
